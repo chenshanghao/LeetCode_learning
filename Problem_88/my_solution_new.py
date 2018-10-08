b02eabc60597540397e44924aa7e5729c66d1246
21551021@zju.edu.cn <21551021@zju.edu.cn>
2018-10-07 23:52:04 -0700

    10_7_1

---
+++ Problem_88/my_solution_new.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def merge(self, nums1, m, nums2, n):
+        """
+        :type nums1: List[int]
+        :type m: int
+        :type nums2: List[int]
+        :type n: int
+        :rtype: void Do not return anything, modify nums1 in-place instead.
+        """
+        i, j = m-1, n-1
+        
+        for k in range(m+n-1, -1, -1):           
+            if i < 0 or j < 0:
+                break
+                
+            if nums1[i] > nums2[j]:
+                nums1[k] = nums1[i]
+                i -= 1
+            else:
+                nums1[k] = nums2[j]
+                j -= 1
+
+        if i < 0:
+            while(k>=0):
+                nums1[k] = nums2[j]
+                k -= 1
+                j -= 1
+            
+        
